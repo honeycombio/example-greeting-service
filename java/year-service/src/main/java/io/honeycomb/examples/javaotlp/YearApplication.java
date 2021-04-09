727fdbb65f38f4d3fd00988c05b82aa43f108668
Shelby Spees <shelby@honeycomb.io>
2021-04-08 21:15:36 -0700

    Add Honeycomb-OpenTelemetry to Java year service
    - Add path to local .jar to gradle
    - Initialize HoneycombSdk in YearApplication
    
    Note that this doesn't generate events for the Java service yet.

--- java/year-service/src/main/java/io/honeycomb/examples/javaotlp/YearApplication.java
+++ java/year-service/src/main/java/io/honeycomb/examples/javaotlp/YearApplication.java
@@ -8,8 +8,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import io.honeycomb.opentelemetry.HoneycombSdk;
+import io.honeycomb.opentelemetry.sdk.trace.samplers.DeterministicTraceSampler;
+
 @SpringBootApplication
 public class YearApplication {
+
+	@Bean // auto-injects dependencies into your controllers
+	public HoneycombSdk getHoneycomb() {
+		return HoneycombSdk.builder()
+    .setApiKey(System.getenv("HONEYCOMB_WRITE_KEY"))
+    .setDataset(System.getenv("HONEYCOMB_DATASET"))
+    .setEndpoint("https://api.honeycomb.io")
+    .setSampler(new DeterministicTraceSampler(5)) // optional - defaults  to "always sample"
+    .build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(YearApplication.class, args);
