d9306178252f2373fc832c788c5a4e804cad2283
Shelby Spees <shelby@honeycomb.io>
2021-04-08 22:36:48 -0700

    Remove noisy CommandLineRunner from Java year service

--- java/year-service/src/main/java/io/honeycomb/examples/javaotlp/YearApplication.java
+++ java/year-service/src/main/java/io/honeycomb/examples/javaotlp/YearApplication.java
@@ -27,16 +27,4 @@ public class YearApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YearApplication.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-		};
-	}
 }
