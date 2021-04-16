b07b4015828edc38fd450ff4e7dec6457efef6eb
Shelby Spees <shelby@honeycomb.io>
2021-04-16 12:55:35 -0700

    Initialize HoneycombSdk

--- java/year-service/src/main/java/io/honeycomb/examples/javaotlp/YearApplication.java
+++ java/year-service/src/main/java/io/honeycomb/examples/javaotlp/YearApplication.java
@@ -1,10 +1,15 @@
-package io.honeycomb.examples.javaotlp;
+package io.honeycomb.examples.javaotlp; // why do we need this?
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.honeycomb.opentelemetry.HoneycombSdk;
+
 @SpringBootApplication
 public class YearApplication {
+
+	HoneycombSdk honeycomb = new HoneycombSdk.Builder().setApiKey(System.getenv("HONEYCOMB_API_KEY"))
+			.setDataset(System.getenv("HONEYCOMB_DATASET")).setServiceName("year-service-java").build();
 
 	public static void main(String[] args) {
 		SpringApplication.run(YearApplication.class, args);
