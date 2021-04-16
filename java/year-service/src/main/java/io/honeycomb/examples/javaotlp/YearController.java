48edb9e28f7fa5a9285e2de12ac9be3faf0f3289
Shelby Spees <shelby@honeycomb.io>
2021-04-16 14:07:43 -0700

    Add custom instrumentation using vanilla OTel SDK

--- java/year-service/src/main/java/io/honeycomb/examples/javaotlp/YearController.java
+++ java/year-service/src/main/java/io/honeycomb/examples/javaotlp/YearController.java
@@ -6,6 +6,8 @@ import java.util.Random;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.opentelemetry.api.trace.Span;
+
 @RestController
 public class YearController {
 
@@ -14,9 +16,14 @@ public class YearController {
 
 	@RequestMapping("/year")
 	public String index() {
+		Span span = Span.current();
+
+		// import statement stays the same for both vanilla OTel and honey OTel
+		span.setAttribute("import", "import io.opentelemetry.api.trace.Span;");
+
 		int rnd = generator.nextInt(YEARS.length);
-    return YEARS[rnd];
+		String year = YEARS[rnd];
+		span.setAttribute("year", year);
+		return year;
 	}
-
-
 }
