3378a5157ff4acd34881377c7650fe0831ad5122
Vera Reynolds <verareynolds@honeycomb.io>
2021-05-24 18:48:33 -0600

    add year controller, ignore temp stuff

---
+++ dotnet/year-service/Controllers/YearController.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace year_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YearController : ControllerBase
+    {
+        private static readonly int[] Years = new[]
+        {
+            2015, 2016, 2017, 2018, 2019, 2020
+        };
+
+
+        [HttpGet]
+        public int Get()
+        {
+            var rng = new Random();
+            var i = rng.Next(0, 5);
+            return Years[i];
+        }
+    }
+}
