94e8274fc83aba5387cfb529f8ec86bd8b6e1976
Vera Reynolds <verareynolds@honeycomb.io>
2021-05-25 18:27:02 -0600

    send data to honey api

--- dotnet/year-service/Startup.cs
+++ dotnet/year-service/Startup.cs
@@ -1,9 +1,11 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
 namespace year_service
@@ -20,17 +22,24 @@ namespace year_service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "year_service", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "year_service", Version = "v1"});
             });
 
             services.AddOpenTelemetryTracing((builder => builder
+                .SetResourceBuilder(ResourceBuilder.CreateDefault()
+                    .AddService(this.Configuration.GetValue<string>("Otlp:ServiceName")))
                 .AddAspNetCoreInstrumentation()
                 .AddHttpClientInstrumentation()
-                .AddConsoleExporter()));
+                .AddOtlpExporter(options =>
+                {
+                    options.Endpoint = new Uri(Configuration.GetValue<string>("Otlp:Endpoint"));
+                    var apiKey = Configuration.GetValue<string>("Otlp:ApiKey");
+                    var dataset = Configuration.GetValue<string>("Otlp:Dataset");
+                    options.Headers = $"x-honeycomb-team={apiKey},x-honeycomb-dataset={dataset}";
+                })));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,10 +58,7 @@ namespace year_service
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
