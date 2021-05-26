b5f362dc1f312b241bea9cd405f8085d2356dcc1
Vera Reynolds <verareynolds@honeycomb.io>
2021-05-25 18:01:57 -0600

    add otel instrumentation

--- dotnet/year-service/Program.cs
+++ dotnet/year-service/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace year_service
 {
@@ -18,9 +12,6 @@ namespace year_service
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
--- dotnet/year-service/Startup.cs
+++ dotnet/year-service/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using OpenTelemetry.Trace;
 
 namespace year_service
 {
@@ -32,6 +26,11 @@ namespace year_service
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "year_service", Version = "v1" });
             });
+
+            services.AddOpenTelemetryTracing((builder => builder
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddConsoleExporter()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
