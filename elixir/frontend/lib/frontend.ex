7ec571e4b73f5f81be13b8d641ce43f62f41501d
Alex Vondrak <alexvondrak@honeycomb.io>
2021-06-30 15:36:31 -0700

    Add custom spans to the Elixir frontend service
    This appears to break the traces, because the root span from
    opentelemetry_phoenix never gets sent. Don't know what's up with that,
    but I figured I'd commit this to have a reproducible example.

--- elixir/frontend/lib/frontend.ex
+++ elixir/frontend/lib/frontend.ex
@@ -1,11 +1,19 @@
 defmodule Frontend do
+  require OpenTelemetry.Tracer, as: Tracer
+
   def name do
-    endpoint = System.get_env("NAME_ENDPOINT", "http://localhost:8000")
-    HTTPoison.get!("#{endpoint}/year").body
+    Tracer.with_span "✨ call /name ✨" do
+      endpoint = System.get_env("NAME_ENDPOINT", "http://localhost:8000")
+      headers = :otel_propagator.text_map_inject([])
+      HTTPoison.get!("#{endpoint}/name", headers).body
+    end
   end
 
   def message do
-    endpoint = System.get_env("MESSAGE_ENDPOINT", "http://localhost:9000")
-    HTTPoison.get!("#{endpoint}/message").body
+    Tracer.with_span "✨ call /message ✨" do
+      endpoint = System.get_env("MESSAGE_ENDPOINT", "http://localhost:9000")
+      headers = :otel_propagator.text_map_inject([])
+      HTTPoison.get!("#{endpoint}/message", headers).body
+    end
   end
 end
--- elixir/frontend/lib/frontend_web/controllers/greetings_controller.ex
+++ elixir/frontend/lib/frontend_web/controllers/greetings_controller.ex
@@ -1,9 +1,13 @@
 defmodule FrontendWeb.GreetingsController do
   use FrontendWeb, :controller
 
+  require OpenTelemetry.Tracer, as: Tracer
+
   def index(conn, _params) do
     name = Frontend.name()
     message = Frontend.message()
-    text(conn, "Hello #{name}, #{message}")
+    Tracer.with_span "🎨 render message ✨" do
+      text(conn, "Hello #{name}, #{message}")
+    end
   end
 end
