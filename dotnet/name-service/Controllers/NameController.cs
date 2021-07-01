0493920b76d0b4ae8a88bc5343f80e25a4b7d090
Robb Kidd <robbkidd@honeycomb.io>
2021-07-01 18:59:58 -0400

    dockerize name-dotnet

--- dotnet/name-service/Controllers/NameController.cs
+++ dotnet/name-service/Controllers/NameController.cs
@@ -28,6 +28,17 @@ namespace name_service.Controllers
             {2020, new[] {"olivia", "noah", "emma", "liam", "ava", "elijah", "isabella", "oliver", "sophia", "lucas"}},
         };
 
+        private static string GetYearEndpoint()
+        {
+            var yearEndpoint = Environment.GetEnvironmentVariable("YEAR_ENDPOINT");
+            if (yearEndpoint == null)
+            {
+                return "http://localhost:6001/year";
+            } else {
+                return yearEndpoint + "/year";
+            }
+        }
+
 
         [HttpGet]
         public async Task<string> GetAsync()
@@ -36,7 +47,7 @@ namespace name_service.Controllers
             current?.AddTag("apple", 1);
             current?.AddBaggage("avocado", "12");
 
-            var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:6001/year");
+            var request = new HttpRequestMessage(HttpMethod.Get, GetYearEndpoint());
             var client = _clientFactory.CreateClient();
             var response = await client.SendAsync(request);
             if (!response.IsSuccessStatusCode) return "OH NO!";
