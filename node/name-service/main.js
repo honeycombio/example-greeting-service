db3abf223f0f07a6776809099ae73b7a4ac38552
JamieDanielson <jamieedanielson@gmail.com>
2021-08-04 09:34:39 -0400

    Adds w3c interop for mixed beeline and OTEL (#52)

--- node/name-service/main.js
+++ node/name-service/main.js
@@ -1,11 +1,15 @@
 'use strict';
-const beeline = require('honeycomb-beeline')({
+const beeline = require('honeycomb-beeline');
+
+beeline({
   // Get this via https://ui.honeycomb.io/account after signing up for Honeycomb
   writeKey: `${process.env.HONEYCOMB_API_KEY}`,
   // The name of your app is a good choice to start with
   dataset: `${process.env.HONEYCOMB_DATASET}`,
   serviceName: `${process.env.SERVICE_NAME}` || 'node-name-service',
   apiHost: `${process.env.HONEYCOMB_API}` || 'https://api.honeycomb.io',
+  httpTraceParserHook: beeline.w3c.httpTraceParserHook,
+  httpTracePropagationHook: beeline.w3c.httpTracePropagationHook
 });
 
 const express = require('express');
