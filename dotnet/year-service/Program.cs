4bf501f0103ff018869fc4438a6b18a1be7fa043
Vera Reynolds <verareynolds@honeycomb.io>
2021-11-09 12:46:11 -0700

    year: upgrade dotnet, use minimal API

--- dotnet/year-service/Program.cs
+++ dotnet/year-service/Program.cs
@@ -1,17 +1,42 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
 
-namespace year_service
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+const string activitySourceName = "honeycomb.examples.year-service-dotnet";
+ActivitySource activitySource = new(activitySourceName);
+
+builder.Services.AddOpenTelemetryTracing(providerBuilder => providerBuilder
+    .SetResourceBuilder(ResourceBuilder.CreateDefault()
+        .AddService(builder.Configuration.GetValue<string>("Otlp:ServiceName"))
+        .AddEnvironmentVariableDetector()
+    )
+    .AddSource(activitySourceName)
+    .AddAspNetCoreInstrumentation()
+    .AddHttpClientInstrumentation()
+    .AddOtlpExporter(options =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        options.Endpoint = new Uri(builder.Configuration.GetValue<string>("Otlp:Endpoint"));
+        var apiKey = builder.Configuration.GetValue<string>("Otlp:ApiKey");
+        var dataset = builder.Configuration.GetValue<string>("Otlp:Dataset");
+        options.Headers = $"x-honeycomb-team={apiKey},x-honeycomb-dataset={dataset}";
+    }));
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
-}
+var app = builder.Build();
+int[] years =
+{
+    2015, 2016, 2017, 2018, 2019, 2020
+};
+
+app.MapGet("/year", () =>
+{
+    using var activity = activitySource.StartActivity("🗓 get-a-year ✨");
+    activity?.SetTag("banana", 1);
+    var rng = new Random();
+    var i = rng.Next(years.Length - 1);
+    var year = years[i];
+    return year;
+});
+
+app.Run();
