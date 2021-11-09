27e346a1cd45b3514f5350c873ca5f5e398eb859
Vera Reynolds <verareynolds@honeycomb.io>
2021-11-09 10:44:19 -0700

    frontend: bump beestro

--- dotnet/frontend/Program.cs
+++ dotnet/frontend/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
-builder.Services.UseHoneycomb(builder.Configuration);
+builder.Services.AddHoneycomb(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
