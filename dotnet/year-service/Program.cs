6f71df838674055b79b586048bb56e96cee1e411
Purvi Kanal <purvikanal@honeycomb.io>
2022-11-09 13:27:29 -0500

    add otel logs for year service

--- dotnet/year-service/Program.cs
+++ dotnet/year-service/Program.cs
@@ -1,25 +1,35 @@
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Exporter;
 
 var builder = WebApplication.CreateBuilder(args);
 
 const string telemetrySourceName = "honeycomb.examples.year-service-dotnet";
 
-builder.Services.AddOpenTelemetryTracing(providerBuilder => providerBuilder
-    .SetResourceBuilder(ResourceBuilder.CreateDefault()
-        .AddService(builder.Configuration.GetValue<string>("Otlp:ServiceName"))
-        .AddEnvironmentVariableDetector()
-    )
+var resourceBuilder = ResourceBuilder.CreateDefault()
+    .AddService(builder.Configuration.GetValue<string>("Otlp:ServiceName"))
+    .AddEnvironmentVariableDetector();
+
+var configureOtlpExporter = (OtlpExporterOptions options) =>
+{
+    options.Endpoint = new Uri(builder.Configuration.GetValue<string>("Otlp:Endpoint"));
+    var apiKey = builder.Configuration.GetValue<string>("Otlp:ApiKey");
+    var dataset = builder.Configuration.GetValue<string>("Otlp:Dataset");
+    options.Headers = $"x-honeycomb-team={apiKey},x-honeycomb-dataset={dataset}";
+};
+
+builder.Services.AddOpenTelemetryTracing(options => options
+    .SetResourceBuilder(resourceBuilder)
     .AddSource(telemetrySourceName)
     .AddAspNetCoreInstrumentation()
     .AddHttpClientInstrumentation()
-    .AddOtlpExporter(options =>
-    {
-        options.Endpoint = new Uri(builder.Configuration.GetValue<string>("Otlp:Endpoint"));
-        var apiKey = builder.Configuration.GetValue<string>("Otlp:ApiKey");
-        var dataset = builder.Configuration.GetValue<string>("Otlp:Dataset");
-        options.Headers = $"x-honeycomb-team={apiKey},x-honeycomb-dataset={dataset}";
-    }));
+    .AddOtlpExporter(configureOtlpExporter));
+
+builder.Logging.AddOpenTelemetry(options => options
+    .SetResourceBuilder(resourceBuilder)
+    .AddOtlpExporter(configureOtlpExporter)
+);
 
 Tracer tracer = TracerProvider.Default.GetTracer(telemetrySourceName);
 var app = builder.Build();
@@ -35,6 +45,7 @@ app.MapGet("/year", () =>
     var rng = new Random();
     var i = rng.Next(years.Length - 1);
     var year = years[i];
+    app.Logger.LogInformation($"Selected year: ${year}");
     return year;
 });
 
