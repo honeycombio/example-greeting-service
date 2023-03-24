0fa06c781a10c09c3bb1f4c62689ddeb123cf956
Purvi Kanal <purvikanal@honeycomb.io>
2023-03-24 13:37:10 -0400

    swtich to startSpan and create parent span

--- web/src/web-vitals-instrumentation.js
+++ web/src/web-vitals-instrumentation.js
@@ -1,46 +1,53 @@
 import { onFID, onLCP, onCLS, onINP, onTTFB } from 'web-vitals';
-import { InstrumentationBase, InstrumentationConfig } from '@opentelemetry/instrumentation';
-import { trace } from '@opentelemetry/api';
+import { InstrumentationBase } from '@opentelemetry/instrumentation';
+import { trace, context } from '@opentelemetry/api';
 import { hrTime } from '@opentelemetry/core';
 
 export class WebVitalsInstrumentation extends InstrumentationBase {
-  onReport(metric) {
+  onReport(metric, parentSpanContext) {
     const now = hrTime();
-    trace
+    const webVitalsSpan = trace
       .getTracer('web-vitals-instrumentation')
-      .startActiveSpan(metric.name, { startTime: now }, (span) => {
-        span.setAttributes({
-          [`webvitals.${metric.name}.name`]: metric.name,
-          [`webvitals.${metric.name}.id`]: metric.id,
-          [`webvitals.${metric.name}.navigationType`]: metric.navigationType,
-          [`webvitals.${metric.name}.delta`]: metric.delta,
-          [`webvitals.${metric.name}.rating`]: metric.rating,
-          [`webvitals.${metric.name}.value`]: metric.value,
-          // can expand these into their own attributes!
-          [`webvitals.${metric.name}.entries`]: JSON.stringify(metric.entries),
-        });
-        span.end();
-      });
+      .startSpan(metric.name, { startTime: now }, parentSpanContext);
+
+    webVitalsSpan.setAttributes({
+      [`webvitals.${metric.name}.name`]: metric.name,
+      [`webvitals.${metric.name}.id`]: metric.id,
+      [`webvitals.${metric.name}.navigationType`]: metric.navigationType,
+      [`webvitals.${metric.name}.delta`]: metric.delta,
+      [`webvitals.${metric.name}.rating`]: metric.rating,
+      [`webvitals.${metric.name}.value`]: metric.value,
+      // can expand these into their own attributes!
+      [`webvitals.${metric.name}.entries`]: JSON.stringify(metric.entries),
+    });
+    webVitalsSpan.end();
   }
+
   enable() {
     if (this.enabled) {
       return;
     }
     this.enabled = true;
+
+    // create a parent span that will have all web vitals spans as children
+    const parentSpan = trace.getTracer('web-vitals-instrumentation').startSpan('web-vitals');
+    const ctx = trace.setSpan(context.active(), parentSpan);
+    parentSpan.end();
+
     onFID((metric) => {
-      this.onReport(metric);
+      this.onReport(metric, ctx);
     });
     onCLS((metric) => {
-      this.onReport(metric);
+      this.onReport(metric, ctx);
     });
     onLCP((metric) => {
-      this.onReport(metric);
+      this.onReport(metric, ctx);
     });
     onINP((metric) => {
-      this.onReport(metric);
+      this.onReport(metric, ctx);
     });
     onTTFB((metric) => {
-      this.onReport(metric);
+      this.onReport(metric, ctx);
     });
   }
 }
