1a0cbd6aa66c132e731da43375cb4a25561174ea
Vera Reynolds <verareynolds@honeycomb.io>
2023-09-12 17:39:48 -0400

    fix: close new context when using baggage
    makeCurrent() creates a closeable Scope
    if we don't close it, context can get wonky and mess up span relationships and trace association
    java otel recommends using try-with-resource block

--- java/frontend/src/main/java/io/honeycomb/examples/frontend_java/GreetingController.java
+++ java/frontend/src/main/java/io/honeycomb/examples/frontend_java/GreetingController.java
@@ -1,43 +1,43 @@
 package io.honeycomb.examples.frontend_java;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
+import io.opentelemetry.api.baggage.Baggage;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.opentelemetry.api.baggage.Baggage;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.Tracer;
+import java.net.URISyntaxException;
 
 @RestController
 public class GreetingController {
-	@Autowired
-	private NameService nameService;
+    @Autowired
+    private NameService nameService;
 
-	@Autowired
-	private MessageService messageService;
+    @Autowired
+    private MessageService messageService;
 
-	@Autowired
-	private Tracer tracer;
+    @Autowired
+    private Tracer tracer;
 
-	@RequestMapping("/greeting")
-	public String index() throws URISyntaxException, IOException, InterruptedException {
-		String name = nameService.getName();
+    @RequestMapping("/greeting")
+    public String index() throws URISyntaxException {
+        String name = nameService.getName();
+        Span.current().setAttribute("app.username", name);
 
-		Span.current().setAttribute("app.username", name);
-		Baggage.current()
-			.toBuilder()
-			.put("app.username", name)
-			.build()
-			.makeCurrent();
+        try (final Scope ignored = Baggage.current()
+            .toBuilder()
+            .put("app.username", name)
+            .build()
+            .makeCurrent()
+        ) {
+            String message = messageService.getMessage();
 
-		String message = messageService.getMessage();
-
-		Span span = tracer.spanBuilder("🎨 render message ✨").startSpan();
-		String greeting = String.format("Hello, %s, %s", name, message);
-		span.end();
-		return greeting;
-	}
+            Span span = tracer.spanBuilder("🎨 render message ✨").startSpan();
+            String greeting = String.format("Hello, %s, %s", name, message);
+            span.end();
+            return greeting;
+        }
+    }
 }
