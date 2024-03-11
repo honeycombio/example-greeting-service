b5d445c33e9d135263c856c2840a32069d05673f
JamieDanielson <jamieedanielson@gmail.com>
2024-03-11 12:34:30 -0400

    add logging to python year service

--- python/year-service/yearservice/yearapp/apps.py
+++ python/year-service/yearservice/yearapp/apps.py
@@ -1,12 +1,27 @@
+import logging
 import os
 from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import \
+from opentelemetry._logs import set_logger_provider
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
     OTLPSpanExporter
-from opentelemetry.sdk.resources import Resource, SERVICE_NAME
+)
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
-    ConsoleSpanExporter,
+    ConsoleSpanExporter
+)
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import (
+    OTLPLogExporter
+)
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import (
+    BatchLogRecordProcessor,
+    ConsoleLogExporter,
+    SimpleLogRecordProcessor
+)
+from opentelemetry.sdk.resources import (
+    Resource,
+    SERVICE_NAME
 )
 from django.apps import AppConfig
 
@@ -16,18 +31,43 @@ class YearappConfig(AppConfig):
 
     def ready(self):
         trace.set_tracer_provider(TracerProvider(
-            resource=Resource.create({SERVICE_NAME: "year-python"})
+            resource=Resource.create({SERVICE_NAME: 'year-python'})
         ))
 
-        # helpful to see in console while developing
+        trace_exporter = OTLPSpanExporter(
+            headers=(
+                    ('x-honeycomb-team', os.environ.get('HONEYCOMB_API_KEY')),),
+            endpoint=os.environ.get('HONEYCOMB_API_ENDPOINT',
+                                    'https://api.honeycomb.io')
+        )
+
         trace.get_tracer_provider().add_span_processor(
             BatchSpanProcessor(ConsoleSpanExporter())
         )
 
         trace.get_tracer_provider().add_span_processor(
-            BatchSpanProcessor(OTLPSpanExporter(
-                headers=(
-                    ("x-honeycomb-team", os.environ.get("HONEYCOMB_API_KEY")),),
-                endpoint=os.environ.get("HONEYCOMB_API_ENDPOINT",
-                                        "https://api.honeycomb.io")
-            )))
+            BatchSpanProcessor(trace_exporter))
+
+        logger_provider = LoggerProvider(
+            resource=Resource.create({SERVICE_NAME: 'year-python'})
+        )
+        set_logger_provider(logger_provider)
+
+        log_exporter = OTLPLogExporter(
+            headers=(
+                    ('x-honeycomb-team', os.environ.get('HONEYCOMB_API_KEY')),),
+            endpoint=os.environ.get('HONEYCOMB_API_ENDPOINT',
+                                    'https://api.honeycomb.io')
+        )
+
+        logger_provider.add_log_record_processor(
+            SimpleLogRecordProcessor(ConsoleLogExporter()))
+
+        logger_provider.add_log_record_processor(
+            BatchLogRecordProcessor(log_exporter)
+        )
+        logger = logging.getLogger('my-logger')
+        logger.addHandler(LoggingHandler(
+            level=logging.INFO, logger_provider=logger_provider))
+
+        logger.setLevel(logging.INFO)
--- python/year-service/yearservice/yearapp/views.py
+++ python/year-service/yearservice/yearapp/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 import random
+import logging
+
+logger = logging.getLogger('my-logger')
 
 years = [2015, 2016, 2017, 2018, 2019, 2020]
 
 
 def year(request):
-    return HttpResponse(random.choice(years))
+    year = random.choice(years)
+    logger.info({'selected_year': year})
+    return HttpResponse(year)
