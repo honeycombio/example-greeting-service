81109a1cf4e5577a5e3e2d640a72a9dd15324fc5
Jamie Danielson <jamiedanielson@honeycomb.io>
2024-03-11 16:09:43 -0400

    feat: convert python year service to otel, add logs, update deps (#973)
    ## Which problem is this PR solving?
    
    - closes #971 
    - closes a buncha dependabots
    
    ## Short description of the changes
    
    - convert python year service to otel
    - add otel logs to year service (to log selected year to show on active
    span)
    - update dependencies in all python services
    - add log pipeline to otel-collector-config
    
    ## How to verify it has the expected result
    
    `tilt up python` from root directory, should see log for selected year
    in console output, collector log output, and honeycomb
    
    ![python selected year
    log](https://github.com/honeycombio/example-greeting-service/assets/29520003/3e40cd6b-27b6-427f-8bbf-545ba9852dee)

--- python/year-service/yearservice/yearapp/apps.py
+++ python/year-service/yearservice/yearapp/apps.py
@@ -1,24 +1,74 @@
-import os
 import logging
-import beeline
-import beeline.propagation.w3c as w3c
-
+import os
+from opentelemetry import trace
+from opentelemetry._logs import set_logger_provider
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+    OTLPSpanExporter
+)
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter
+)
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import (
+    OTLPLogExporter
+)
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import (
+    BatchLogRecordProcessor,
+    ConsoleLogExporter,
+    SimpleLogRecordProcessor
+)
+from opentelemetry.sdk.resources import (
+    Resource,
+    SERVICE_NAME
+)
 from django.apps import AppConfig
-
 
 
 class YearappConfig(AppConfig):
     name = 'yearapp'
 
     def ready(self):
-        # If you use uwsgi, gunicorn, celery, or other pre-fork models, see the section below on pre-fork
-        # models and do not initialize here.
-        beeline.init(
-            writekey=os.environ.get("HONEYCOMB_API_KEY"),
-            api_host=os.environ.get("HONEYCOMB_API_ENDPOINT",
-                                "https://api.honeycomb.io"),
-            service_name='year-python',
-            debug=True,
-            http_trace_parser_hook=w3c.http_trace_parser_hook,
-            http_trace_propagation_hook=w3c.http_trace_propagation_hook,
+        resource = Resource.create({SERVICE_NAME: 'year-python'})
+        headers = (('x-honeycomb-team', os.environ.get('HONEYCOMB_API_KEY')),)
+        endpoint = os.environ.get(
+            'HONEYCOMB_API_ENDPOINT', 'https://api.honeycomb.io')
+
+        trace.set_tracer_provider(TracerProvider(
+            resource=resource
+        ))
+
+        trace_exporter = OTLPSpanExporter(
+            headers=headers,
+            endpoint=endpoint
         )
+
+        trace.get_tracer_provider().add_span_processor(
+            BatchSpanProcessor(ConsoleSpanExporter())
+        )
+
+        trace.get_tracer_provider().add_span_processor(
+            BatchSpanProcessor(trace_exporter))
+
+        logger_provider = LoggerProvider(
+            resource=resource
+        )
+        set_logger_provider(logger_provider)
+
+        log_exporter = OTLPLogExporter(
+            headers=headers,
+            endpoint=endpoint
+        )
+
+        logger_provider.add_log_record_processor(
+            SimpleLogRecordProcessor(ConsoleLogExporter()))
+
+        logger_provider.add_log_record_processor(
+            BatchLogRecordProcessor(log_exporter)
+        )
+        logger = logging.getLogger('my-logger')
+        logger.addHandler(LoggingHandler(
+            level=logging.INFO, logger_provider=logger_provider))
+
+        logger.setLevel(logging.INFO)
--- python/year-service/yearservice/yearservice/settings.py
+++ python/year-service/yearservice/yearservice/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'beeline.middleware.django.HoneyMiddleware'
 ]
 
 ROOT_URLCONF = 'yearservice.urls'
