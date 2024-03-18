9d51eb6a931de510b0191c5f8afa710c00e2a84e
JamieDanielson <jamieedanielson@gmail.com>
2024-03-18 17:53:17 -0400

    we dont even need the builder anymore

--- java/year-service/src/main/java/io/honeycomb/examples/javaotlp/YearApplication.java
+++ java/year-service/src/main/java/io/honeycomb/examples/javaotlp/YearApplication.java
@@ -6,16 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import io.opentelemetry.instrumentation.log4j.appender.v2_17.OpenTelemetryAppender;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-
 @SpringBootApplication
 public class YearApplication {
 
     public static void main(String[] args) {
-        OpenTelemetrySdk sdk = OpenTelemetrySdk.builder()
-                .build();
-        OpenTelemetryAppender.install(sdk);
         SpringApplication.run(YearApplication.class, args);
     }
 
