01f5e51130ab7615bb6d291eefccce643ab46ead
Vera Reynolds <verareynolds@honeycomb.io>
2025-04-03 11:41:10 -0400

    simulate a random error

--- java/message-service/src/main/java/io/honeycomb/examples/message/MessageEndpoints.java
+++ java/message-service/src/main/java/io/honeycomb/examples/message/MessageEndpoints.java
@@ -3,6 +3,9 @@ package io.honeycomb.examples.message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.*;
+import java.util.Random;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(value = "message")
@@ -12,6 +15,9 @@ public class MessageEndpoints {
 
     @GetMapping(produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
     public String message() {
+        if (new Random().nextDouble() < 0.2) { // 20% chance of failure
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Simulated server error");
+        }
         return messageService.getMessage();
     }
 }
